bc8e39106d1adb6d14efbdb4d089880ceae67454
sup8369 <sup8369@gmail.com>
2020-01-28 01:36:14 +0900

    Fix typo

--- actions/index.js
+++ actions/index.js
@@ -4,17 +4,17 @@ export const EDIT_TODO = "EDIT_TODO";
 export const COMPLETE_TODO = "COMPLETE_TODO";
 
 export function addTodo(title, date) {
-  return { type: types.ADD_TODO, title, date };
+  return { type: ADD_TODO, title, date };
 }
 
 export function deleteTodo(id) {
-  return { type: types.DELETE_TODO, id };
+  return { type: DELETE_TODO, id };
 }
 
 export function editTodo(id, title, date) {
-  return { type: types.EDIT_TODO, id, title, date };
+  return { type: EDIT_TODO, id, title, date };
 }
 
 export function completeTodo(id) {
-  return { type: types.COMPLETE_TODO, id };
+  return { type: COMPLETE_TODO, id };
 }
